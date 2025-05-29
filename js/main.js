d21753a7b1dfecc2506d23f7ac687f7e5b992d9b
Daniel1104-rv <rodriguezdelvalledaniel1104@gmail.com>
2025-05-29 09:33:57 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -63,8 +63,8 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 async function enviarMensaje(mensaje) {
-  const urlGPT = "http://98.84.53.237/api-gpt-php/endpoints/chat.php";
-  const urlIOT = "http://98.84.53.237/iot-api-php/controllers/AddIotDevice.php";
+  const urlGPT = "http://3.237.65.87/api-gpt-php/endpoints/chat.php";
+  const urlIOT = "http://3.237.65.87/iot-api-php/controllers/AddIotDevice.php";
 
   const datos = { message: mensaje };
   const outputText = document.getElementById("outputText");
